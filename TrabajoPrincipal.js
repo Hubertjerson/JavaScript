efc1f15c8aeadc81adef2a5e80fbaf043d334789
Jerson Huaman <jerson.huaman.11@outlook.com>
2021-09-12 20:11:36 -0300

    Cambios

--- TrabajoPrincipal.js
+++ TrabajoPrincipal.js
@@ -1,20 +1,44 @@
 
+var usuario = prompt('Hola , Desea calcular algun numero "si o no"');
+if(usuario ==='si'){
+    var usuarioRespuesta = prompt('Que desea calcular "+" ,"-" ,"*" ')
+    switch (usuarioRespuesta) {
+        case '+':
+            var resultadoNumero = suma();
+            alert('La respuesta es'+ resultadoNumero);
+            break;
+        case '-':
+            var resultadoNumero = restar();
+            alert('La respuesta es'+ resultadoNumero);
+            break;
+        case '*':
+            alert('La respuesta es'+ resultadoNumero);
+            var resultadoNumero = multiplicar();
+            break;
+        default:
+            alert('Gracias por Usar la Calculadora');
+            break;
+    }
+}
 
-var personaNombre = prompt('ingrese su nombre');
-var personaProducto = prompt('Los productos disponibles son agua - leche - bebidas');
 
-switch (personaProducto) {
-    case 'agua':
-        alert('La persona' + ' ' + personaNombre + 'compro' + ' ' + personaProducto + ' ' + 'Muchas Gracias');
-        break;
-    case 'leche':
-        alert('La persona' + ' ' + personaNombre + 'compro' + ' ' + personaProducto + ' ' + 'Muchas Gracias');
-        break;
-    case 'bebida':
-        alert('La persona' + ' ' + personaNombre + 'compro' + ' ' + personaProducto + ' ' + 'Muchas Gracias');
-        break;
-    default:
-        alert('No puso ningun producto');
-        alert('Gracias por la visita');
-        break;
+
+function suma(){
+    var numero1 = prompt('ingrese numero');
+    var numero2 = prompt ('ingrese segundo numero');
+    var resultadoNumero = parseInt(numero1)+parseInt(numero2);
+    return resultadoNumero;
+}
+
+function restar(){
+    var numero1 = prompt('ingrese numero');
+    var numero2 = prompt ('ingrese segundo numero');
+    var resultadoNumero = numero1 - numero2;
+    return resultadoNumero;
+}
+function multiplicar(){
+    var numero1 = prompt('ingrese numero');
+    var numero2 = prompt ('ingrese segundo numero');
+    var resultadoNumero = numero1 * numero2;
+    return resultadoNumero;
 }
